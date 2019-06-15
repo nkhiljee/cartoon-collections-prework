e26b1d8604ed79bce9e4faae1030dde13a9bded7
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 21:42:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    if cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
-    end
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
