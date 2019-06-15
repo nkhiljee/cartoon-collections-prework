9b4755e1867460e69f3a8994d830092bf7812061
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 21:30:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.capitalize do |
+  array.capitalize do |word|
+    
 end
 
 def long_planeteer_calls# code an argument here
