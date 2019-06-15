813e2571fff842b52936dac72b555eefb0910852
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-15 21:33:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+ words.any? do |word|
+  word.length > 4
 end
 
 def find_the_cheese# code an argument here
